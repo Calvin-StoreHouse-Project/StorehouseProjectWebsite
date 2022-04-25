363360630012ffd26fb99058df9fe6d8dda72599
unknown <mindysport@gmail.com>
2022-04-25 19:31:45 -0400

    updated website

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - One Page Wonder v6.0.4 (https://calvin-cs396-98-storehouse.github.io/storehouse/)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
 // This file is intentionally blank
